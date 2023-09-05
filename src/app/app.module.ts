a97bade83a43261574238c716487f8e8a0230cda
Michael Gichuhi <gmikekamau@gmail.com>
2023-09-05 14:30:53 +0300

    🎉 began projects by creating primary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home/home-page/home-page.component';
+import { RestaurantsPageComponent } from './pages/restaurants/restaurants-page/restaurants-page.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { AboutComponent } from './pages/about/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    RestaurantsPageComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/restaurants/restaurants-page/restaurants-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-page',
+  templateUrl: './restaurants-page.component.html'
+})
+export class RestaurantsPageComponent {
+
+}
