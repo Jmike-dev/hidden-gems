4d86ca96050402ec333ff0d7b82bc0c9112c8dbc
Michael Gichuhi <gmikekamau@gmail.com>
2023-09-07 18:20:19 +0300

    📱 responsive home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,27 +8,34 @@ import { RestaurantsPageComponent } from './pages/restaurants/restaurants-page/r
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { AboutComponent } from './pages/about/about/about.component';
 import { RouterModule ,Routes} from '@angular/router';
-
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
 // defining routes
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'restaurants', component: RestaurantsPageComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
+// setting up lottie
+export function playerFactory() {
+  return player;
+}
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     RestaurantsPageComponent,
     NavbarComponent,
-    AboutComponent
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
