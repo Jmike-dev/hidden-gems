233b12c3886328a7f0b9ec50b132b8278129bc7e
Michael Gichuhi <gmikekamau@gmail.com>
2024-06-10 20:06:05 +0300

    ⚰️ removed dead code

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,18 +1,14 @@
 // import { AnimationOptions } from '@angular/animations';
 import { Component } from '@angular/core';
-// import { AnimationItem } from 'lottie-web';
-import { AnimationOptions } from 'ngx-lottie/lib/symbols';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
+  
 })
 export class HomePageComponent {
-  // This is the option that uses the package's AnimationOption interface
-  options: AnimationOptions = {
-    path: '../../../../assets/lottie/right-arrow.json',
-  };
-
+ 
   constructor() {}
 
 
