fd1bd3fc9a26e058a759d3493aedf4261636753a
Michael Gichuhi <gmikekamau@gmail.com>
2024-06-24 13:05:15 +0300

    🚧 working on the dialog

--- src/app/utils/recipe-dialog/recipe-dialog.component.ts
+++ src/app/utils/recipe-dialog/recipe-dialog.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-dialog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-dialog.component.html'
 })
 export class RecipeDialogComponent {
+  visible:boolean = false;
+
+  showDialog(){
+    this.visible = true;
+  }
 
 }
