8d2beb4ec465b9ad9199a934112703e4f0fe0b00
Michael Gichuhi <gmikekamau@gmail.com>
2024-06-28 23:22:50 +0300

    🚚 added children routes

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,13 +1,18 @@
-import { Routes } from "@angular/router";
-import { AboutComponent } from "./app/pages/about/about/about.component";
-import { HomePageComponent } from "./app/pages/home/home-page/home-page.component";
-import { MealsComponent } from "./app/pages/meals/meals.component";
-import { RestaurantsPageComponent } from "./app/pages/restaurants/restaurants-page/restaurants-page.component";
+import {Routes} from '@angular/router';
+import {AboutComponent} from './app/pages/about/about/about.component';
+import {HomePageComponent} from './app/pages/home/home-page/home-page.component';
+import {MealsComponent} from './app/pages/meals/meals.component';
+import {RestaurantsPageComponent} from './app/pages/restaurants/restaurants-page/restaurants-page.component';
+import {RecipeInfoPageComponent} from './app/utils/recipe-info-page/recipe-info-page.component';
 
- export const routes: Routes = [
+export const routes: Routes = [
     {path: 'home', component: HomePageComponent},
     {path: 'about', component: AboutComponent},
     {path: 'restaurants', component: RestaurantsPageComponent},
-    {path: 'meals', component: MealsComponent},
+    {
+        path: 'meals',
+        component: MealsComponent,
+        children: [{path: 'recipe-info', component: RecipeInfoPageComponent}],
+    },
     {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
