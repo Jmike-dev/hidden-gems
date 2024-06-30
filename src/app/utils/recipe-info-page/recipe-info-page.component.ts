3d67723b631c4769124426efd890d74a466a6287
Michael Gichuhi <gmikekamau@gmail.com>
2024-06-30 20:41:21 +0300

    🚧 created the recipe info component

---
+++ src/app/utils/recipe-info-page/recipe-info-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-info-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-info-page.component.html'
+})
+export class RecipeInfoPageComponent {
+
+}
