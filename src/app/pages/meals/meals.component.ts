0252dffe88c2c383d481050c2f4ad2f4878707fc
Michael Gichuhi <gmikekamau@gmail.com>
2024-07-09 16:18:28 +0300

    ⚰️ removed dead code

--- src/app/pages/meals/meals.component.ts
+++ src/app/pages/meals/meals.component.ts
@@ -1,11 +1,9 @@
-import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 import { FoodsService } from 'src/app/services/foods.service';
 import { RecipeInstance } from 'src/interfaces/RecipeItems';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
 import { RestaurantsPageComponent } from '../restaurants/restaurants-page/restaurants-page.component';
-import { AppDialogComponent } from 'src/app/utils/app-dialog/app-dialog.component';
 import { MealInfoCardComponent } from 'src/app/utils/meal-info-card/meal-info-card.component';
 @Component({
     selector: 'app-meals',
@@ -14,17 +12,12 @@ import { MealInfoCardComponent } from 'src/app/utils/meal-info-card/meal-info-ca
         CommonModule,
         ReactiveFormsModule,
         RestaurantsPageComponent,
-        AppDialogComponent,
         MealInfoCardComponent,
     ],
     standalone: true,
-    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MealsComponent {
-    readonly dialog = inject(MatDialog);
-    openRecipeDialog() {
-        this.dialog.open(AppDialogComponent);
-    }
+
     constructor(private foodService: FoodsService) {}
     appForm = new FormGroup({
         mealInput: new FormControl(''),
