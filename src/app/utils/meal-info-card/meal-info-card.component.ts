01031dd05f8ae7aaf0f772b936e8234ce6abf94d
Michael Gichuhi <gmikekamau@gmail.com>
2024-07-10 17:19:57 +0300

    👽️ subscribed to API for ingredients

--- src/app/utils/meal-info-card/meal-info-card.component.ts
+++ src/app/utils/meal-info-card/meal-info-card.component.ts
@@ -1,21 +1,40 @@
-import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AppDialogComponent } from '../app-dialog/app-dialog.component';
+import { IngredientsInstance } from 'src/interfaces/RecipeItems';
+import { FoodsService } from 'src/app/services/foods.service';
 
 @Component({
     selector: 'app-meal-info-card',
     standalone: true,
-    imports: [AppDialogComponent],
+    imports: [],
     templateUrl: './meal-info-card.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MealInfoCardComponent {
+    constructor(
+        private foodService: FoodsService,
+        public dialog: MatDialog
+    ) {}
     @Input() MealImage = '';
     @Input() MealTitle = '';
     @Input() MealId = 0;
 
-    readonly dialog = inject(MatDialog);
-    openRecipeDialog() {
-        this.dialog.open(AppDialogComponent);
+    ingredientInfo: IngredientsInstance[] | undefined;
+
+    openRecipeDialog(recipeTitle: string, recipeInstructions: string) {
+        this.dialog.open(AppDialogComponent, {
+            data: {
+                recipeTitle: recipeTitle,
+                recipeInstructions: recipeInstructions,
+            },
+        });
+    }
+    showData() {
+        this.foodService.searchIngredients(this.MealId).subscribe((data) => {
+            this.ingredientInfo = data.extendedIngredients;
+            console.log(data.instructions);
+            this.openRecipeDialog(data.title, data.instructions);
+        });
     }
 }
